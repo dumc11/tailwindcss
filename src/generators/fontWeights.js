6c75b9cd4b1b418ea7ed9e3cb84021dffdff06d8
dumconstantin <dum.constantin@gmail.com>
2017-12-23 18:19:44 +0200

    Add extra styles for bold and normal font weights.

--- src/generators/fontWeights.js
+++ src/generators/fontWeights.js
@@ -2,9 +2,24 @@ import _ from 'lodash'
 import defineClass from '../util/defineClass'
 
 export default function({ fontWeights, naming: { fontWeights: ns } }) {
-  return _.map(fontWeights, (weight, modifier) => {
+  let weights = _.map(fontWeights, (weight, modifier) => {
     return defineClass(`${ns.base}${ns.modifierPrefix}${modifier}`, {
       'font-weight': `${weight}`,
     })
   })
+  if (ns.normal) {
+    weights.push(
+      defineClass(ns.normal, {
+        'font-weight': 'normal',
+      })
+    )
+  }
+  if (ns.bold) {
+    weights.push(
+      defineClass(ns.bold, {
+        'font-weight': 'bold',
+      })
+    )
+  }
+  return weights
 }
