3f16c1956c9cde57a5e922c286faa0ddcb16392b
dumconstantin <dum.constantin@gmail.com>
2017-12-26 02:58:49 +0200

    Fix display

---
+++ src/generators/display.tachyons.js
@@ -0,0 +1,36 @@
+import defineClasses from '../util/defineClasses'
+
+export default function({ naming: { display: ns } }) {
+  return defineClasses({
+    [ns.block]: {
+      display: 'block',
+    },
+    [ns.inlineBlock]: {
+      display: 'inline-block',
+    },
+    [ns.inline]: {
+      display: 'inline',
+    },
+    [ns.table]: {
+      display: 'table',
+    },
+    [ns.tableRow]: {
+      display: 'table-row',
+    },
+    [ns.tableCell]: {
+      display: 'table-cell',
+    },
+    [ns.hidden]: {
+      display: 'none',
+    },
+    [ns.tableRowGroup]: {
+      display: 'table-row-group',
+    },
+    [ns.tableColumn]: {
+      display: 'table-column',
+    },
+    [ns.tableColumnGroup]: {
+      display: 'table-column-group',
+    },
+  })
+}
