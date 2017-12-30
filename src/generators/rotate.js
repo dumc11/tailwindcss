4c5a85783f2961f3c6bf890d6295cc07d9557a71
dumconstantin <dum.constantin@gmail.com>
2017-12-30 17:23:55 +0200

    Fix rotate class.

--- src/generators/rotate.js
+++ src/generators/rotate.js
@@ -4,7 +4,7 @@ import defineClass from '../util/defineClass'
 export default function({ rotate, naming: { rotate: ns } }) {
   return _.map(rotate, (size, modifier) => {
     return defineClass(`${ns.base}${ns.modifierPrefix}${modifier}`, {
-      rotate: `${size}`,
+      transform: `rotate(${size}deg)`,
     })
   })
 }
